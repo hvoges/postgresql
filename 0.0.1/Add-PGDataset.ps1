4136d166d28fd2f767195ddc7d82a96e517401c0
Holger Voges <holger.voges@netz-weise.de>
2024-05-19 14:27:05 +0200

    Changed Parameter order

--- 0.0.1/Add-PGDataset.ps1
+++ 0.0.1/Add-PGDataset.ps1
@@ -1,71 +1,7 @@
 Function Add-PGDataset {
-    <#
-.SYNOPSIS
-    Inserts data into a PostgreSQL table.
 
-.DESCRIPTION
-    The Add-PgsDataset function allows for the insertion of data into a PostgreSQL table by directly providing values or column-value pairs. This function supports connections to PostgreSQL using Npgsql, facilitating operations with databases through pipeline input or parameterized inputs.
-
-.PARAMETER Server
-    The hostname or IP address of the PostgreSQL server. Defaults to "localhost".
-
-.PARAMETER Port
-    The port on which the PostgreSQL server is listening. Defaults to 5432.
-
-.PARAMETER Database
-    The name of the PostgreSQL database to connect to. This parameter is mandatory.
-
-.PARAMETER Credential
-    The PSCredential object containing the username and password for database authentication.
-
-.PARAMETER Datasource
-    An Npgsql.NpgsqlDataSource object representing the data source. If not provided, it will be created using other connection parameters.
-
-.PARAMETER Table
-    The name of the table to insert data into. This parameter is mandatory.
-
-.PARAMETER Values
-    An array of values to be inserted into the table. Each value in the array represents a single row's value for the columns in the table. This parameter is mandatory when using the 'Values' parameter set.
-
-.PARAMETER ColumnValuePairs
-    A hashtable where each key-value pair represents a column name and its corresponding value to be inserted. This parameter is mandatory when using the 'ColumnValuePairs' parameter set.
-
-.EXAMPLE
-    $credential = Get-Credential
-    $values = @('John', 'Doe', 30)
-    Add-PgsDataset -Server 'db.example.com' -Database 'mydb' -Credential $credential -Table 'users' -Values $values
-
-    This example inserts a single row into the 'users' table with the values provided in the $values array.
-
-.EXAMPLE
-    $credential = Get-Credential
-    $columnValuePairs = @{
-        FirstName = 'John'
-        LastName = 'Doe'
-        Age = 30
-    }
-    Add-PgsDataset -Server 'db.example.com' -Database 'mydb' -Credential $credential -Table 'users' -ColumnValuePairs $columnValuePairs
-
-    This example inserts a single row into the 'users' table using the column-value pairs provided in the $columnValuePairs hashtable.
-
-.NOTES
-    This function requires the Npgsql library for connecting to and working with PostgreSQL databases. Ensure that this library is available and loaded into your PowerShell session.
-
-    The function supports inserting data by specifying either an array of values (assuming the order matches the table columns) or a hashtable of column-value pairs for more precise control over the inserted data.
-#>
-    
     [CmdletBinding(DefaultParameterSetName = 'Values')]
     Param(
-        [string]$Server = "localhost",
-        
-        [string]$Port = 5432, 
-        
-        [string]$Database,
-
-        [PSCredential]$Credential,
-
-        [Npgsql.NpgsqlDataSource]$Datasource = $Script:Datasource,
-
         [Parameter(Mandatory = $true)]
         [String]$Table,
 
@@ -77,24 +13,25 @@ Function Add-PGDataset {
             ParameterSetName = 'Values',
             ValueFromPipeline,
             ValueFromPipelineByPropertyName)]
-        # An Array with all Values to be inserted. If only some columns are to be inserted, use the ColumnValuePairs parameter
+        # An Array with all Values to be inserted. 
         $Values,
-        #[Object]$Values, 
 
-        [Parameter(Mandatory,
-            ParameterSetName = 'ColumnValuePairs',
-            ValueFromPipeline,
-            ValueFromPipelineByPropertyName)]
-        # A Hashtable containing the ColumnName and its Value
-        [hashtable]$ColumnValuePairs
-        # [String[]]$Columns = '*'
+        [string]$Server = "localhost",
+        
+        [string]$Port = 5432, 
+        
+        [string]$Database = $Script:Database,
+
+        [PSCredential]$Credential,
+
+        [Npgsql.NpgsqlDataSource]$Datasource = $Script:Datasource
     )
 
     Begin {
         if ( $Values -isnot [System.Collections.Hashtable] ) {
             $DBStrings = Format-PGString -TableName $Table -ColumnName $Columns           
         }
-        If (-not $Datasource ) {
+        If ( $Database -and $Credential ) {
             $ConnectionString = @{
                 Host     = $Server
                 Port     = $Port
@@ -104,6 +41,9 @@ Function Add-PGDataset {
             }
             $Datasource = [Npgsql.NpgsqlDataSource]::Create($ConnectionString)
         }
+        Elseif ( -not $Datasource ) {
+            Throw 'No connection information provided'
+        }
         $Connection = $Datasource.OpenConnectionAsync()
     }
 
@@ -111,11 +51,11 @@ Function Add-PGDataset {
         if (( $Values -is [System.Collections.Hashtable] ) -or ( $Values -is [System.Collections.Specialized.OrderedDictionary])) {
 # ColumnValuePairs checken
             $DBStrings = Format-PGString -TableName $Table -ColumnName $Values.keys 
-            $ValueList = ( 1..$ColumnValuePairs.keys.Count | ForEach-Object { '$' + $_ } ) -join ',' 
-            $Query = 'INSERT INTO {0} ({1}) VALUES ({2});' -f $DBStrings.Table, $DBStrings.Columns, $ValueList
+            $ValueList = ( 1..$Values.keys.Count | ForEach-Object { '$' + $_ } ) -join ',' 
+            $Query = 'INSERT INTO {0} ({1}) VALUES ({2});' -f $DBStrings.TableFullName, $DBStrings.Columns, $ValueList
             Write-Verbose -Message "Query: $Query"
             $Command = [npgsql.NpgsqlCommand]::new($Query, $Connection.Result)
-            Foreach ( $Value in $ColumnValuePairs.Values ) {
+            Foreach ( $Value in $Values.Values ) {
                 $Param = $Command.CreateParameter()
                 $Param.Value = $Value
                 $null = $Command.Parameters.Add($Param)  
@@ -130,18 +70,22 @@ Function Add-PGDataset {
         {
             $ColumnString = '(' + ($Columns -join ',') + ')'            
             Switch ( $Values ) {
-                { $_ -is [System.Data.DataRow] } { $Values = $Values.itemarray }
-                { $_ -is [array] } { }
-                { $_ -is [PSObject] } { $Values = $Values.PSObject.Properties.Value }
+                { $_ -is [System.Data.DataRow] } { $Values = $Values.itemarray; break }
+                { $_ -is [array]               } { break }
+                { $_ -is [PSObject]            } { $Values = $Values.PSObject.Properties.Value; break }
 
                 Default { Write-Error 'Data cannot be converted'; Continue }
             }
-            if ($Columns.Count -ne $Values.Count) {
+            if ( $Columns.Count -gt 1 -and $Values -is [string] ) {
+                Throw 'Values must be an array when inserting multiple columns'
+            }
+            elseif ( $Columns.Count -ne $Values.Count ) {
                 Throw 'The number of columns and values must match'
             }
             $ValueList = ( 1..$Values.Count | ForEach-Object { '$' + $_ } ) -join ',' 
-            $Query = 'Insert into {0} {1} values({2});' -f $DBStrings.Table,$ColumnString,$ValueList
+            $Query = 'Insert into {0} {1} values({2});' -f $DBStrings.TableFullName,$ColumnString,$ValueList
             $Command = [npgsql.NpgsqlCommand]::new($Query, $Connection.Result)
+            $TableColumns = Get-PGColumnDefinition -Table $DBStrings.TableFullName -Datasource $Datasource -KeepOpen
             Foreach ($Value in $Values) {
                 $Param = $Command.CreateParameter()
                 if ( $Value -eq "Default") {
