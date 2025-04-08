20d2d8c862871b0ad38d86847438f26e28fd7d7b
Holger Voges <holger.voges@netz-weise.de>
2025-04-08 22:49:15 +0200

    changed parameter-order

--- 0.0.1/Get-PGDatabaseTable.ps1
+++ 0.0.1/Get-PGDatabaseTable.ps1
@@ -1,6 +1,63 @@
 function Get-PGDatabaseTable {
+<#
+.SYNOPSIS
+    Retrieves information about tables in a PostgreSQL database.
+
+.DESCRIPTION
+    The Get-PGDatabaseTable function queries the information_schema.tables view in a PostgreSQL database
+    to retrieve information about base tables. By default, it excludes system tables from the pg_catalog 
+    and information_schema schemas.
+
+.PARAMETER Database
+    The name of the database to query.
+
+.PARAMETER Table
+    The name of a specific table to query. If not specified, all tables will be returned.
+    Note: This parameter is defined but not currently used in the function implementation.
+
+.PARAMETER ShowViews
+    If specified, includes views in the results.
+    Note: This parameter is defined but not currently used in the function implementation.
+
+.PARAMETER ComputerName
+    The hostname or IP address of the PostgreSQL server. Defaults to "localhost".
+    Used with the OnLink parameter set.
+
+.PARAMETER Port
+    The TCP port on which the PostgreSQL server is listening. Defaults to 5432.
+    Used with the OnLink parameter set.
+
+.PARAMETER Credential
+    A PSCredential object containing the username and password to connect to the PostgreSQL server.
+    Required when using the OnLink parameter set.
+
+.PARAMETER Datasource
+    An existing Npgsql.NpgsqlDataSource object representing a connection to a PostgreSQL server.
+    Required when using the Connection parameter set.
+
+.EXAMPLE
+    Get-PGDatabaseTable -Database "mydb" -Datasource $myDataSource
+    
+    Returns all tables in the "mydb" database using an existing connection.
+
+.EXAMPLE
+    Get-PGDatabaseTable -Database "mydb" -ComputerName "pgserver" -Credential $cred
+    
+    Returns all tables in the "mydb" database on the remote PostgreSQL server "pgserver".
+
+.NOTES
+    This function requires the Npgsql .NET data provider to be installed.
+    It can use either an existing connection or create a new one based on provided parameters.
+#>    
     [CmdletBinding()]
     Param(        
+        [Parameter(mandatory)]
+        [string]$Database,
+
+        [string]$Table,
+
+        [Switch]$ShowViews,
+
         [Parameter(ParameterSetName = 'OnLink',
             ValueFromPipeline = $true,   
             ValueFromPipelineByPropertyName = $true)]
@@ -13,16 +70,7 @@ function Get-PGDatabaseTable {
         [PSCredential]$Credential,
 
         [Parameter(ParameterSetName = 'Connection')]    
-        $Datasource = $Script:Datasource,
-
-        [Parameter()]
-        [string]$Database,
-
-        [Parameter()]
-        [string]$Table,
-
-        [Parameter()]
-        [Switch]$ShowViews
+        $Datasource = $Script:Datasource
     )   
     Begin {
         If ( $PSCmdlet.ParameterSetName -eq 'OnLink') {
@@ -35,7 +83,7 @@ function Get-PGDatabaseTable {
             $Datasource = [Npgsql.NpgsqlDataSource]::Create($ConnectionString)
         }
         Elseif ( -not $Datasource ) {
-            Throw 'Please connect to a PostgreSQL server first using Connect-PGDatabase or provide a Datasource object or connection parameters.'
+            Throw 'Please connect to a PostgreSQL server first using Connect-PGServer or provide a Datasource object or connection parameters.'
         }
     }
 
