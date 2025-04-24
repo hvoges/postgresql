672471298d46b4db85ddcd73c4cc6d7cdda8c9d0
NWFnort <holger.voges@netz-weise.de>
2025-04-24 15:19:30 +0200

    Fixed Credential-Lost when Database was added

--- 0.1.0/Use-PGDatabase.ps1
+++ 0.1.0/Use-PGDatabase.ps1
@@ -8,6 +8,8 @@ function Use-PGDatabase {
         $ConnectionString = ( $Script:Datasource.ConnectionString.split(";") | Out-String )
         $ConnectionStringDictionary = ConvertFrom-StringData -StringData $ConnectionString
         $ConnectionStringDictionary["Database"] = $Database
+        $ConnectionStringDictionary["Username"] = $script:ConnectionCredential.username
+        $ConnectionStringDictionary["Password"] = $script:ConnectionCredential.GetNetworkCredential().Password
         $Script:Datasource = [Npgsql.NpgsqlDataSource]::Create($ConnectionStringDictionary)
     } else {
         $Script:Database = $Database
