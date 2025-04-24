43ed57c43b7ea59dd6641c884dc6d573a4438e98
NWFnort <holger.voges@netz-weise.de>
2025-04-24 15:15:15 +0200

    initial version

---
+++ 0.1.0/Add-PGSqlLibraries.ps1
@@ -0,0 +1,37 @@
+Function Add-PGSqlLibraries {
+    [CmdletBinding()]
+    Param()
+
+    if ( $PSVersionTable.PSVersion.Major -le 5 ) {  
+        $global:AssemblyPath = "$PSScriptRoot\npgsql\netstandard2\"
+        Add-Type -Path "$AssemblyPath\Microsoft.Bcl.AsyncInterfaces.dll"
+        Add-Type -Path "$AssemblyPath\Microsoft.Bcl.HashCode.dll"
+        Add-Type -Path "$AssemblyPath\Microsoft.Extensions.Logging.Abstractions.dll"
+        Add-Type -Path "$AssemblyPath\Npgsql.dll"
+        Add-Type -Path "$AssemblyPath\System.Buffers.dll"
+        Add-Type -Path "$AssemblyPath\System.Collections.Immutable.dll"
+        Add-Type -Path "$AssemblyPath\System.Diagnostics.DiagnosticSource.dll"
+        Add-Type -Path "$AssemblyPath\System.Memory.dll"
+        Add-Type -Path "$AssemblyPath\System.Numerics.Vectors.dll"
+        Add-Type -Path "$AssemblyPath\System.Runtime.CompilerServices.Unsafe.dll"
+        Add-Type -Path "$AssemblyPath\System.Text.Json.dll"
+        Add-Type -Path "$AssemblyPath\System.Threading.Channels.dll"
+        Add-Type -Path "$AssemblyPath\System.Threading.Tasks.Extensions.dll"
+
+        $Handler = {
+            param($Sender, $e)
+            $RequestedName = [System.Reflection.AssemblyName]::new($e.Name)
+            if ($RequestedName.Name -eq "System.Runtime.CompilerServices.Unsafe") {
+                return [System.Reflection.Assembly]::LoadFrom("$AssemblyPath\System.Runtime.CompilerServices.Unsafe.dll")
+            }
+            elseif ($RequestedName.Name -eq "System.Buffers") {
+                return [System.Reflection.Assembly]::LoadFrom("$AssemblyPath\System.Buffers.dll")
+            }
+            return $null
+        }
+        [AppDomain]::CurrentDomain.Add_AssemblyResolve($Handler)
+    }
+    Elseif ( $PSVersionTable.PSVersion.Major -gt 5 ) {
+        
+    }
+}
